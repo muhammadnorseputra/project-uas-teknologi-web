78bdd9d53adcb6c53fa0a80add986982fc4b01f4
Putrabungsu6 <muhammadnorseputra@gmail.com>
2024-06-11 13:02:52 +0800

    add section

--- lib/connection.php
+++ lib/connection.php
@@ -1,3 +1,16 @@
 <?php
+class Connection {
+    
+    protected $db;
 
-$db = new PDO('sqlite:database.sqlite');
+    public function __construct() {
+        $this->db = new PDO('sqlite:MyProfile.db');
+    }
+
+    public function getProfile($key) {
+        // fetch profile information
+        $tbl_profile = $this->db->query("SELECT * FROM profile WHERE key = '$key'");
+        $profile = $tbl_profile->fetchObject();
+        return $profile->value;
+    }
+}
